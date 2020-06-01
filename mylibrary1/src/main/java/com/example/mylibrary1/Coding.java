f02cff090400f56462585c5e5d44e1f62e731a16
cstanALC <cstan.alc@gmail.com>
2020-06-01 11:56:30 +0800

    commit first

--- mylibrary1/src/main/java/com/example/mylibrary1/Coding.java
+++ mylibrary1/src/main/java/com/example/mylibrary1/Coding.java
@@ -1,4 +1,12 @@
 package com.example.mylibrary1;
 
 public class Coding {
+    private int plus(int x,int y){
+        int total=0;
+        total=x+y;
+
+        return total;
+    }
+
+
 }
--- mylibrary2/src/main/java/com/example/mylibrary2/Calculation.java
+++ mylibrary2/src/main/java/com/example/mylibrary2/Calculation.java
@@ -1,4 +1,10 @@
 package com.example.mylibrary2;
 
 public class Calculation {
+    private int mult(int x,int y){
+        int total=0;
+        total=x*y;
+
+        return total;
+    }
 }
